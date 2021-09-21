d20e024d6bea3ab7c62c48c717ab56bf9995dabf
JacobVaerst23 <816769@mystma.org>
2021-09-21 09:26:38 -0500

    topFiveMovies Attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def topFiveMovies(a , b , c , d , e):
+    return(a +" "+ b +" " +c +" " +d +" "+ e)
+
+print(topFiveMovies("Fast and Furious 5", "Fast and Furious 7", "Star Wars Series", "Jumanji", "Suicide Squad"))
